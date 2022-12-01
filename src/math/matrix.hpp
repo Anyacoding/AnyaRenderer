ff8298c99d3d56c113f278d3c4eb1c5ab3ef5aa6
Anyacoding <1710273192@qq.com>
2022-12-01 23:58:14 +0800

    update the matrix.hpp

--- src/math/matrix.hpp
+++ src/math/matrix.hpp
@@ -6,6 +6,7 @@
 #define ANYA_ENGINE_MATRIX_HPP
 
 #include "vec.hpp"
+#include <exception>
 
 namespace anya {
 
@@ -86,15 +87,41 @@ public:
 
 public:
 #pragma region 矩阵运算
+    // 转置
+    constexpr Matrix<N, M> transpose() const {
+        Matrix<N, M> ret{};
+        for (int i = 0; i < M; ++i) {
+            for (int j = 0; j < N; ++j) {
+                ret(j, i) = (*this)(i, j);
+            }
+        }
+        return ret;
+    }
 
+    // 行列式
+    [[nodiscard]] constexpr numberType det() const {
+        if (M != N) {
+            throw std::length_error("This is not a square matrix, so you can't find the determinant!");
+        }
+        if constexpr (M == 1) {
+            return (*this)(0, 0);
+        }
+        else {
+            numberType ans = 0;
+            for (int i = 0; i < M; ++i) {
+                ans += (*this)(0, i) * minor_det(0, i);
+            }
+            return ans;
+        }
+    }
 #pragma endregion
 
 public:
 #pragma region 矩阵运算涉及的运算符重载
-    // 矩阵乘法
+    // 矩阵 * 矩阵
     template<int C>
     constexpr friend Matrix<M, C>
-    operator*(const Matrix<M, N>& lhs, const Matrix<N, C>& rhs) {
+    operator*(const Matrix<M, N>& lhs, const Matrix<N, C>& rhs) noexcept {
         Matrix<M, C> ret{};
         for (int i = 0; i < M; ++i) {
             for (int j = 0; j < C; ++j) {
@@ -108,11 +135,64 @@ public:
 
     // 列向量左乘矩阵
     constexpr friend Vector<M>
-    operator*(const Matrix<M, N>& lhs, const Vector<N>& rhs) {
+    operator*(const Matrix<M, N>& lhs, const Vector<N>& rhs) noexcept {
         Vector<M> ret{};
         for (int i = 0; i < M; ++i) ret[i] = lhs.rowVec(i).dot(rhs);
         return ret;
     }
+
+    // TODO: 暂时不打算提供向量右乘矩阵，因为这里默认向量都是列向量了，右乘应该是用不上的
+
+    // 矩阵加法
+    constexpr Matrix&
+    operator+=(const Matrix& rhs) noexcept {
+        for (int i = 0; i < N; ++i) {
+            data[i] += rhs.data[i];
+        }
+        return *this;
+    }
+
+    // 矩阵减法
+    constexpr Matrix&
+    operator-=(const Matrix& rhs) noexcept {
+        for (int i = 0; i < N; ++i) {
+            data[i] -= rhs.data[i];
+        }
+        return *this;
+    }
+
+    // 矩阵乘k
+    constexpr Matrix&
+    operator*=(numberType k) noexcept {
+        for (int i = 0; i < N; ++i) {
+            data[i] *= k;
+        }
+        return *this;
+    }
+
+    // 矩阵除k
+    constexpr Matrix&
+    operator/=(numberType k) {
+        for (int i = 0; i < N; ++i) {
+            data[i] /= k;
+        }
+        return *this;
+    }
+
+    constexpr friend Matrix
+    operator+(const Matrix& lhs, const Matrix& rhs) noexcept { return Matrix{lhs} += rhs; }
+
+    constexpr friend Matrix
+    operator-(const Matrix& lhs, const Matrix& rhs) noexcept { return Matrix{lhs} -= rhs; }
+
+    constexpr friend Matrix
+    operator*(const Matrix& lhs, numberType k) noexcept { return Matrix{lhs} *= k; }
+
+    constexpr friend Matrix
+    operator*(numberType k, const Matrix& rhs) noexcept { return Matrix{rhs} *= k; }
+
+    constexpr friend Matrix
+    operator/(const Matrix& lhs, numberType k) { return Matrix{lhs} /= k; }
 
 #pragma endregion
 
@@ -135,6 +215,28 @@ public:
     constexpr static bool
     out_range(int i, int j) {
         return i < 0 || i >= M || j < 0 || j >= N;
+    }
+
+    // matrix去掉x行和y列后得到的余子式
+    // fixed: 返回值必须用auto, 否则 Matrix<1 - 1, 1 - 1> 时无法满足约束
+    constexpr auto minor(int x, int y) const {
+        Matrix<M - 1, N - 1> ret{};
+        for (int i = 0, row = 0; i < M; ++i) {
+            if (i == x) continue ;
+            for (int j = 0, column = 0; j < N; ++j) {
+                if (j == y) continue ;
+                ret(row, column) = (*this)(i, j);
+                ++column;
+            }
+            ++row;
+        }
+        return ret;
+    }
+
+    // 代数余子式的值
+    [[nodiscard]] constexpr numberType
+    minor_det(int x, int y) const {
+        return minor(x, y).det() * ((x + y) % 2 ? -1 : 1);
     }
 #pragma endregion
 
